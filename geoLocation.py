0a24f5aab895aeb9520294ca5d9d6e52a602b3ad
Finnian Anderson <finnian@fxapi.co.uk>
2014-10-02 10:20:30 +0100

    Update and rename geolocation.py to geoLocation.py

--- geoLocation.py
+++ geoLocation.py
@@ -1,14 +1,9 @@
 import urllib2
 import json
 
-def geolocation():
+def geoLocation():
      f = urllib2.urlopen('http://freegeoip.net/json/')
      json_string = f.read()
      f.close()
      location = json.loads(json_string)
      return location
-
-city = geolocation['city']
-county = geolocation['region_name']
-country = geolocation['country_name']
-zip = geolocation['zipcode']
